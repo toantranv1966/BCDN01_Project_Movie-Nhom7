7487716fcfbf96561b49b6541b5f12b2609163d4
Toan <toan.tranv1966@gmail.com>
2022-01-12 15:07:30 +0700

    edit admin 1

--- src/pages/_AdminPage/Users/Edit/EditUser.js
+++ src/pages/_AdminPage/Users/Edit/EditUser.js
@@ -37,7 +37,7 @@ const EditUser = (props) => {
         taiKhoan: thongTinTaiKhoan.taiKhoan,
         matKhau: thongTinTaiKhoan.matKhau,
         email: thongTinTaiKhoan.email,
-        soDt: thongTinTaiKhoan.soDt,
+        soDT: thongTinTaiKhoan.soDT,
         "maNhom": "",
         maLoaiNguoiDung: thongTinTaiKhoan.maLoaiNguoiDung,
         hoTen: thongTinTaiKhoan.hoTen
@@ -96,7 +96,7 @@ const EditUser = (props) => {
         <Input type={'email'} name="email" onChange={formik.handleChange} value={formik.values.email} />
       </Form.Item>
       <Form.Item label="Số điện thoại">
-        <Input name="soDt" onChange={formik.handleChange} value={formik.values.soDt}/>
+        <Input name="soDT" onChange={formik.handleChange} value={formik.values.soDT}/>
       </Form.Item>
     <Form.Item label="Loại người dùng">
       <Select options={loaiNguoiDung?.map((lnd,index)=>({label:lnd.tenLoai,value:lnd.maLoaiNguoiDung}))}
